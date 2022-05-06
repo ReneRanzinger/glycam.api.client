a41a5fab460023ab9028a254eb35f623d3a7a22c
Rene Ranzinger <rene@ccrc.uga.edu>
2022-05-06 17:07:04 -0400

    Updating Jackson dependencies

--- src/run/java/org/glycam/api/client/AppRun.java
+++ src/run/java/org/glycam/api/client/AppRun.java
@@ -7,7 +7,7 @@ public class AppRun
     public static void main(String[] args) throws IOException, InterruptedException
     {
         String t_comandLine = "-g ./data/2022.04.10.glycan.csv -o ./data/output/ -v";
-        t_comandLine = "-j ./data/output/jobs.old.json -o ./data/output/ -v";
+        t_comandLine = "-j ./data/output/2022.04.30.jobs.json -o ./data/output/ -v";
         String[] t_args = t_comandLine.split(" ");
         App.main(t_args);
     }
